dd3411e025c7f8266684dcf816ee284492646ee7
Enrique Lozada <Enrique Lozada>
2017-03-16 17:13:49 -0600

    nos lleva la chingada

--- app/makeApointment.php
+++ app/makeApointment.php
@@ -8,7 +8,7 @@
     <body>
         <?php
         require_once "../models/User.php";
-        $paysheet = filter_input(INPUT_POST, 'student');
+        $paysheet = filter_input(INPUT_POST, 'professor');
         if( ! $paysheet )
         {
             header("Location:" . User::baseurl() . "app/list.php");
@@ -16,19 +16,28 @@
         $db = new Database;
         $user = new User($db);
         $user->setId($paysheet);
-        $users = $user->viewStudent();
+        $users = $user->viewHours();
         ?>
         <div class="container">
             <div class="col-lg-12">
                 <h2 class="text-center text-primary">Users List</h2>
-                <h2 class="text-center text-primary">A0<?php echo $paysheet; ?></h2>
-                 <form>
-                   <input type="radio" name="dia" value="Monday" checked> Monday<br>
-                   <input type="radio" name="dia" value="Tuesday"> Tuesday<br>
-                   <input type="radio" name="dia" value="Wednesday"> Wednesday<br>
-                   <input type="radio" name="dia" value="Thursday"> Thursday<br>
-                   <input type="radio" name="dia" value="Friday"> Friday
-                 </form> 
+                <h2 class="text-center text-primary">L0<?php echo $paysheet; ?></h2>
+
+                <form action="saveApointment.php" method="post">
+                        <select name="apointment" class="form-control">
+                            <?php foreach( $users as $user ) {
+                                echo '<option value="'.$user->id.'">'.$user->id.' '.$user->day.' '.$user->start.' '.$user->finish.' '.$user->type. '</option>';
+                            }
+                            ?>
+                        </select>
+                        <br>
+                        <label for="date">Date (year-month-day):</label>
+                        <input type="date" name="date" class="form-control"/>
+                        <br>
+                        <input class="btn btn-success col-lg-2 col-lg-offset-5" value="Continuar" type="submit">
+                    </form>
+
+          
             </div>
         </div>
     </body>
